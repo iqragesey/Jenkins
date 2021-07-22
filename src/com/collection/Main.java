7cb9dfb109b25cd3c82c5e195c096b7c9f9fb56c
iqragesey <72842756+iqragesey@users.noreply.github.com>
2021-07-21 21:11:56 -0400

    first commit

---
+++ src/com/collection/Main.java
@@ -0,0 +1,14 @@
+package com.collection;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Batch 9 is great");
+            Thread.sleep(2000);
+
+
+        }
+    }
+}
